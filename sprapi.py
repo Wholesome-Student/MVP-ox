fd9b4b19f1697f22b22c5612d341486cccf44c8a
kaikaikaito <kaikai.raspi@gmail.com>
2022-09-16 16:56:24 +0900

    update sprapi.py

--- sprapi.py
+++ sprapi.py
@@ -1,22 +1,38 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
+from datetime import datetime
+import os.path
 
-# use creds to create a client to interact with the Google Drive API
+path=os.path.dirname(os.path.abspath(__file__))
+
+#Google Drive API
 scope =['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('spr/sodium-hue-361405-237068a500a2.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name(path+'/sodium-hue-361405-237068a500a2.json', scope)
 client = gspread.authorize(creds)
+s_sheet = client.open("MVP")
+sheet1 = s_sheet.get_worksheet(0)
+sheet2 = s_sheet.get_worksheet(1)
 
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-sheet = client.open("MVP").sheet1
+print("シート%d %d行%d列"%(sheet1.index,sheet1.row_count,sheet1.col_count))
+print("シート%d %d行%d列"%(sheet2.index,sheet2.row_count,sheet2.col_count))
 
-# Extract and print all of the values
-"""
-list_of_hashes = sheet.get_all_records()
-#print(list_of_hashes)
-with open("spr/cell.json", "w") as f:
-    json.dump(list_of_hashes, f, ensure_ascii=False, indent=4)
-"""
+def load_data() -> list:
+    return sheet1.get_all_cells()
 
-sheet.update_cell(1,1,"test1")
+def write_data(data,client_id:int,correct_ans:bool):
+    answers=list(data.values())
+    correct=answers.count(correct_ans)
+    sum=len(answers)
+
+    cells = sheet1.range(1, client_id, 4, client_id)
+    cells[0].value = datetime.now().isoformat()
+    cells[1].value = correct
+    cells[2].value = sum
+    cells[3].value = correct/sum
+
+    sheet1.update_cells(cells)
+
+with open(path+"/ans.json") as f:
+    data=json.load(f)
+    write_data(data,1,False)

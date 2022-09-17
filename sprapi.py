a13ae49bd30319517d600d138cb7375015cbd0d2
kaikaikaito <kaikai.mot@gmail.com>
2022-09-17 18:12:26 +0900

    update sprapi.py

--- sprapi.py
+++ sprapi.py
@@ -6,34 +6,34 @@ import os.path
 path=os.path.dirname(os.path.abspath(__file__))
 
 #Google Drive API
-scope =['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 client = gspread.service_account(filename=path+'/sodium-hue-361405-237068a500a2.json')
-s_sheet = client.open("MVP")
-sheet1 = s_sheet.get_worksheet(0)
-sheet2 = s_sheet.get_worksheet(1)
+spr = client.open("MVP")
+status_sheet = spr.worksheet("status")
+score_sheet = spr.worksheet("score")
+quiz_sheet = spr.worksheet("quiz")
 
-print("シート%d %d行%d列"%(sheet1.index,sheet1.row_count,sheet1.col_count))
-print("シート%d %d行%d列"%(sheet2.index,sheet2.row_count,sheet2.col_count))
+def load_status():
+    return status_sheet.get_values()
 
-def load_ques():
-    return sheet2.get_all_cells()
+def load_score():
+    return score_sheet.get_values()
 
-def load_ans():
-    return sheet1.get_all_cells()
+def load_quiz():
+    return quiz_sheet.get_values()
 
-def write_ans(data,client_id:int,correct_ans:bool):
+def write_score(data,client_id:int,correct_ans:bool):
     answers=list(data.values())
     correct=answers.count(correct_ans)
     sum=len(answers)
 
-    cells = sheet1.range(1, client_id, 4, client_id)
+    cells = score_sheet.range(client_id, 1, client_id, 4)
     cells[0].value = datetime.now().isoformat()
     cells[1].value = correct
     cells[2].value = sum
     cells[3].value = correct/sum
 
-    sheet1.update_cells(cells)
+    score_sheet.update_cells(cells)
 
-with open(path+"/ans.json") as f:
-    data=json.load(f)
-    write_ans(data,1,False)
+#with open(path+"/ans.json") as f:
+#    data=json.load(f)
+#    write_score(data,1,False)

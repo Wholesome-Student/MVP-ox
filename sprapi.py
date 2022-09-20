5f84388ae3ea250d7b7bffb9ff565830e58e2ec9
kaikaikaito <kaikai.mot@gmail.com>
2022-09-20 23:02:02 +0900

    update sprapi.py

--- sprapi.py
+++ sprapi.py
@@ -6,10 +6,10 @@ import os.path
 # このpythonファイルが置いてあるディレクトリ
 path = os.path.dirname(os.path.abspath(__file__))
 
-client_id=None
+client_id = None
 
 # Google Drive API
-client = gspread.service_account(filename=path+'/sodium-hue-361405-237068a500a2.json')
+client = gspread.service_account(filename=path+"/sodium-hue-361405-237068a500a2.json")
 spr = client.open("MVP")
 status_sheet = spr.worksheet("status")
 score_sheet = spr.worksheet("score")
@@ -20,21 +20,22 @@ def init():
     pass
 
 
-def load_status():
-    data = status_sheet.get_values()
-    return {d[0]: d[1] for d in data if d[0]}
+def read_status():
+    data=status_sheet.get_values()
+    return {row[0]: row[1] for row in data if row[0]}
 
-def load_score():
+def read_score():
     return score_sheet.get_values()
 
-def load_quiz():
-    return quiz_sheet.get_all_records()
+def read_quiz():
+    data=quiz_sheet.get_all_records(numericise_ignore=[2])
+    return [{"id":row["id"],"question":row["question"],"answer":row["answer"]==1} for row in data]
 
 
 def write_score(data: dict, correct_ans: bool) -> float:
-    if client_id==None:
-        raise RuntimeError
-    
+    if client_id == None:
+        raise RuntimeError("%s.init() must be run before write." % __name__)
+
     answers = list(data.values())
 
     cells = score_sheet.range(client_id, 1, client_id, 4)
@@ -51,3 +52,22 @@ def write_score(data: dict, correct_ans: bool) -> float:
     score_sheet.update_cells(cells)
 
     return rate
+
+def write_quiz(quizzes: list[dict]):
+    ids = []
+    values = []
+
+    for quiz in quizzes:
+        if "id" not in quiz or "question" not in quiz or "answer" not in quiz:
+            raise ValueError
+        if quiz["id"] not in ids:
+            values.append([quiz["id"], quiz["question"], 1 if quiz["answer"] else 0])
+            ids.append(quiz["id"])
+
+    rows_count = len(ids)+1
+    quiz_range = "A2:C%d"%(rows_count)
+
+    quiz_sheet.resize(rows=rows_count, cols=3)
+    quiz_sheet.batch_update([{"range": quiz_range, "values": values}])
+
+    return ids

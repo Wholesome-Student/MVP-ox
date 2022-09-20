136f89505a0a0013c258e5bc46fa8053cfb60fb5
kaikaikaito <kaikai.mot@gmail.com>
2022-09-20 17:03:32 +0900

    update sprapi.py

--- sprapi.py
+++ sprapi.py
@@ -3,19 +3,20 @@ import json
 from datetime import datetime
 import os.path
 
-#このpythonファイルが置いてあるディレクトリ
-path=os.path.dirname(os.path.abspath(__file__))
+# このpythonファイルが置いてあるディレクトリ
+path = os.path.dirname(os.path.abspath(__file__))
 
-#Google Drive API
+# Google Drive API
 client = gspread.service_account(filename=path+'/sodium-hue-361405-237068a500a2.json')
 spr = client.open("MVP")
 status_sheet = spr.worksheet("status")
 score_sheet = spr.worksheet("score")
 quiz_sheet = spr.worksheet("quiz")
 
+
 def load_status():
-    data=status_sheet.get_values() 
-    return {d[0]:d[1] for d in data if d[0]}
+    data = status_sheet.get_values()
+    return {d[0]: d[1] for d in data if d[0]}
 
 def load_score():
     return score_sheet.get_values()
@@ -23,19 +24,25 @@ def load_score():
 def load_quiz():
     return quiz_sheet.get_values()
 
-def write_score(data,client_id:int,correct_ans:bool):
-    answers=list(data.values())
-    correct=answers.count(correct_ans)
-    sum=len(answers)
+
+def write_score(data: dict, client_id: int, correct_ans: bool) -> float:
+    answers = list(data.values())
 
     cells = score_sheet.range(client_id, 1, client_id, 4)
+
+    correct = cells[1].numeric_value+answers.count(correct_ans)
+    sum = cells[2].numeric_value+len(answers)
+    rate = correct/sum
+
     cells[0].value = datetime.now().isoformat()
     cells[1].value = correct
     cells[2].value = sum
-    cells[3].value = correct/sum
+    cells[3].value = rate
 
     score_sheet.update_cells(cells)
 
-#with open(path+"/ans.json") as f:
+    return rate
+
+# with open(path+"/ans.json") as f:
 #    data=json.load(f)
 #    write_score(data,1,False)
